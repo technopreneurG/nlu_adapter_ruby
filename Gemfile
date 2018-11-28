3883690021c435f8e37923a416fc51aa29374621
Girish <getgirish@gmail.com>
2018-11-29 04:46:32 +0530

    Google Dialogflow: parse functionality implemented

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
 
-#
 #Official AWS Ruby gem for Amazon Lex Runtime Service. This gem is part of the AWS SDK for Ruby.
 gem 'aws-sdk-lex', '~> 1.9'
+
+#google-cloud-dialogflow is the official library for Dialogflow API.
+gem 'google-cloud-dialogflow', '~> 0.2.3'
 
 group :development, :test do
 	gem 'rspec', '3.8.0'
--- lib/nluws/dialogflow.rb
+++ lib/nluws/dialogflow.rb
@@ -1,8 +1,19 @@
+require "google/cloud/dialogflow"
 
 module NLUWS
 	module Adapters
 		class Dialogflow
-			def initialize()
+			def initialize(options = {})
+				@project_id = options[:project_id]
+				@session_id = options[:session_id]
+			end
+
+			def parse(text, language_code = 'en')
+				sessions_client = Google::Cloud::Dialogflow::Sessions.new(version: :v2)
+				formatted_session = Google::Cloud::Dialogflow::V2::SessionsClient.session_path(@project_id, @session_id)
+				query_input = Google::Cloud::Dialogflow::V2::QueryInput.new({text: {language_code: language_code, text: text}})
+				response = sessions_client.detect_intent(formatted_session, query_input)
+				response.query_result.intent.display_name
 			end
 		end
 	end
