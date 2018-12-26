3d5781b430d4df3f17ff466daf028d16eed8927f
Girish <getgirish@gmail.com>
2018-12-26 13:04:57 +0530

    intent & intent collection classes

---
+++ lib/nluws/intent.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+#Class represents Intent in an IntentCollection
+module NLUWSIntent
+	attr_accessor :name, :utterences
+
+	def to_h
+		{
+			:name => @name,
+			:utterences => @utterences
+		}
+	end
+
+	def to_json
+		to_h.to_json
+	end
+end
---
+++ lib/nluws/intent_collection.rb
@@ -0,0 +1,22 @@
+require 'json'
+
+#Class represents a collection of Intents
+module NLUWSIntentCollection
+	attr_accessor :name, :intents
+
+	def initialize(name, intents)
+		@name = name
+		@intents = intents
+	end
+
+	def to_h
+		{
+			:name => @name,
+			:intents => @intents.map { |i| i.to_h }
+		}
+	end
+
+	def to_json
+		to_h.to_json
+	end
+end
