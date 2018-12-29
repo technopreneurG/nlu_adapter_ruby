a96a97006da60d2ea9b6c51e8774c5e69aa67bf0
Girish <getgirish@gmail.com>
2018-12-29 18:33:45 +0530

    fixed lib path

--- lib/nlu_adapter.rb
+++ lib/nlu_adapter.rb
@@ -1,11 +1,11 @@
 require "nlu_adapter/version"
 
 module NluAdapter
-	autoload :NluAdapterIntent, './lib/nlu_adapter/intent'
-	autoload :NluAdapterIntentCollection, './lib/nlu_adapter/intent_collection'
+	autoload :NluAdapterIntent, 'nlu_adapter/intent'
+	autoload :NluAdapterIntentCollection, 'nlu_adapter/intent_collection'
 	module Adapters
-		autoload :Lex,					'./lib/nlu_adapter/lex'
-		autoload :Dialogflow,			'./lib/nlu_adapter/dialogflow'
+		autoload :Lex,					'nlu_adapter/lex'
+		autoload :Dialogflow,			'nlu_adapter/dialogflow'
 	end
 
 	def self.new(name, options = {})
